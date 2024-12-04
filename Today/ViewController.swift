6d740f7d861a966df82a517f44aeda7e737a710a
aallymbekova <alymbekovaaliya@icloud.com>
2024-12-04 23:37:55 +0600

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Aliya Alymbekova on 4/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
